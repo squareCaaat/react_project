e7b0bf21c803baa8a8e816c06a84a8c33dad25e6
squareCaaat <danny79052@gmail.com>
2024-07-04 01:13:57 +0900

    Add CSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({id, title, coverImg, year, summary, genres}){
     return(
-        <div>
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>({year})
-            </h2>
-            <img src={coverImg} alt={title}/>
-            <ul>
-                {genres.map(g=><li key={g}>{g}</li>)}
-            </ul>
-            {/* <p>{summary}</p> */}
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title} className={styles.movie__img} />
+            <div>
+                <h2 className={styles.movie__title}>
+                    <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{year}</h3>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul className={styles.movie__genres}>
+                    {genres.map(g=><li key={g}>{g}</li>)}
+                </ul>
+            </div>
         </div>
     );
 }
