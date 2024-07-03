921db795dfb6b93859abf4a5d1c7fcd79d61c32b
squareCaaat <danny79052@gmail.com>
2024-07-03 23:09:52 +0900

    Add Movie List

--- src/App.js
+++ src/App.js
@@ -4,59 +4,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [selected, setSelected] = useState("");
-  const [gcoin, setGcoin] = useState(0);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-    setGcoin(0);
-    if(amount > 0){
-      console.log("amount is greater than 0");
-      coins.some((coin)=>{
-        if(selected === coin.symbol){
-          setGcoin(amount/Math.round(coin.quotes.USD.price));
-          console.log(gcoin);
-          return true;
-        }
-      });
-    }
-  };
-  const onSelect = (event) => {
-    setSelected(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async ()=>{
+    const item = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )).json();
+    setMovies(item.data.movies);
+    setLoading(false);
   };
   useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then(response=>response.json())
-    .then(data=>{
-      setCoins(data);
-      setLoading(false);
-    });
+    getMovies();
+    console.log(movies);
   },[]);
   return (
     <div>
-      <h1>The Coins {loading? "" : `(${coins.length})`}</h1>
       {loading? <strong>Loading...</strong>:
         <div>
-          <input value={amount} onChange={onChange} type="number" placeholder="Input your budget" /> $ <br/>
-          <select value={selected} onChange={onSelect}>
-            {coins.map((coin)=>{
-              return (
-                <option key={coin.id} value={coin.symbol}>
-                  {coin.name} ({coin.symbol})
-                </option>
-              );
-            })}
-          </select>
-          <h3>
-            You Can Get {gcoin} of 
-              {coins.some((coin)=>{
-                if(coin.symbol === selected){
-                  console.log(selected, "updated!");
-                  return (<span>{coin.name} ({coin.symbol})</span>);
-                }
-              })}
-          </h3>
+          {movies.map((movie)=>(
+            <div key={movie.id}>
+              <h2>{movie.title} ({movie.year})</h2>
+              <img src={movie.medium_cover_image}/>
+              <ul>
+                {movie.genres.map(g=><li key={g}>{g}</li>)}
+              </ul>
+              <p>{movie.summary}</p>
+            </div>
+          ))}  
         </div>
       }
     </div>
